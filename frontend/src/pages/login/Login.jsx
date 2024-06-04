5db57a2be3b5a9767071e3c8255c8989e83bb21c
PEYRAT Aurélien <aure1814fr@gmail.com>
2024-06-04 06:03:30 +0200

    update login

--- frontend/src/pages/login/Login.jsx
+++ frontend/src/pages/login/Login.jsx
@@ -1,28 +1,73 @@
 import "./login.css"
 
+import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+import { login } from '../../redux/actions';
+
 const Login = () => {
+
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [rememberMe, setRememberMe] = useState(false);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    if (!email || !password) {
+      alert('Veuillez remplir tous les champs');
+      return;
+    }
+
+    try {
+      dispatch(login({ email, password, rememberMe }));
+      console.log("sumbited :", email, password, rememberMe)
+      setTimeout(() => {
+        navigate('/profile');
+      }, 500)
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   return (
     <main class="main bg-dark">
       <section class="sign-in-content">
         <i class="fa fa-user-circle sign-in-icon"></i>
         <h2>Sign In</h2>
-        <form>
+        <form onSubmit={handleSubmit}>
           <div class="input-wrapper">
-            <label for="username">Username</label
-            ><input type="text" id="username" />
+            <label for="email">Email</label>
+            <input
+              type="text"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              required />
           </div>
+
           <div class="input-wrapper">
-            <label for="password">Password</label
-            ><input type="password" id="password" />
+            <label for="password">Password</label>
+            <input
+              type="password"
+              id="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              required
+            />
           </div>
-          <div class="input-remember">
-            <input type="checkbox" id="remember-me" />
-            <label for="remember-me">Remember me</label>
+
+          <div className='form-check'>
+            <input
+              type="checkbox"
+              checked={rememberMe}
+              onChange={(e) => setRememberMe(e.target.checked)}
+            />
+            <label>Remember Me</label>
           </div>
-          {/* PLACEHOLDER DUE TO STATIC SITE */}
-          <a href="/profile" class="sign-in-button">Sign In</a>
-          {/* SHOULD BE THE BUTTON BELOW */}
-          {/* <button class="sign-in-button">Sign In</button> */}
+          <button type="submit" class="sign-in-button">Sign In</button>
         </form>
       </section>
     </main>
