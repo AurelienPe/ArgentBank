67e6ac3c6716d7a859e53e907980835e4069a32a
PEYRAT Aurélien <aure1814fr@gmail.com>
2024-06-04 06:05:14 +0200

    update profile

--- frontend/src/pages/profile/Profile.jsx
+++ frontend/src/pages/profile/Profile.jsx
@@ -1,15 +1,75 @@
 import './profile.css'
 import Account from '../../components/account/account'
 import accountData from '../../data/account.json'
+import React, { useState, useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { getProfile, editUsername } from '../../redux/actions';
 
 const Profile = () => {
+  const dispatch = useDispatch(); // 
+
+  const user = useSelector((state) => state.auth.user); // Selectionne "user" dans le reducer
+  const [editing, setEditing] = useState(false); // État pour gérer l'affichage du formulaire
+
+  useEffect(() => {
+    dispatch(getProfile());
+  }, [dispatch]);
+
+  const edit = (e) => {
+    e.preventDefault();
+    setEditing(true); // Mettre à jour l'état pour afficher le formulaire
+  };
+
+  const cancel = (e) => {
+    e.preventDefault();
+    setEditing(false); // Mettre à jour l'état pour revenir au header
+  };
+
+  const save = async (e) => {
+    e.preventDefault();
+    const newUsername = e.target.elements.userName.value;
+    dispatch(editUsername(newUsername));
+    setEditing(false);
+  };
+
   return (
     <main class="main bg-dark">
-      <div class="header">
-        <h1>Welcome back<br />Tony Jarvis!</h1> 
-        <button class="edit-button">Edit Name</button>
-      </div>
-      <h2 class="sr-only">Accounts</h2>
+      {!editing ? (
+      <header class="header">
+        <h1>Welcome back<br />{user.firstName} {user.lastName}!</h1> 
+        <button onClick={edit} class="edit-button">Edit Name</button>
+      </header>
+      ) : (
+        <form className='profil-header' onSubmit={save}>
+          <p>Edit user name</p>
+
+          <label htmlFor="userName">User name :</label>
+          <input
+            type="text"
+            id="userName"
+            defaultValue={user.userName}
+            required
+          />
+          <label htmlFor="firstName">First name</label>
+          <input
+            type="text"
+            id="firstName"
+            defaultValue={user.firstName}
+            disabled
+          />
+          <label htmlFor="lastName">Last name</label>
+          <input
+            type="text"
+            id="lastName"
+            defaultValue={user.lastName}
+            disabled
+          />
+          <section className='profil-buttons'>
+            <button type="submit">Save</button>
+            <button type="button" onClick={cancel}>Cancel</button>
+          </section>
+        </form>
+      )}
 
       {accountData.map((account, index) => (
         <Account
@@ -17,7 +77,6 @@ const Profile = () => {
           title={account.title}
           amount={account.amount}
           description={account.description}
-          transaction={account.transaction}
         />
       ))} 
     </main>
