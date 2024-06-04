6eb559397e509bb2e280c34cf922e59fccc46968
PEYRAT Aurélien <aure1814fr@gmail.com>
2024-06-04 06:04:09 +0200

    update nav

--- frontend/src/components/nav/Nav.jsx
+++ frontend/src/components/nav/Nav.jsx
@@ -1,7 +1,22 @@
 import './nav.css'
 import nav_logo from '../../img/argentBankLogo.webp'
+import { useNavigate } from "react-router-dom";
+import { useSelector, useDispatch } from "react-redux";
+import { logout } from "../../redux/actions";
 
 const Nav = () => {
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const isAuthenticated = useSelector((state) => state.auth.isAuthenticated);
+  const user = useSelector((state) => state.auth.user);
+  console.log(user);
+
+  const handleLogout = async (e) => {
+    e.preventDefault();
+    dispatch(logout());
+    navigate("/login");
+  };
+
   return (
     <nav>
       <a href="/">
@@ -11,14 +26,32 @@ const Nav = () => {
         />
         <h1>Argent Bank</h1>
       </a>
-      <div>
+      {!isAuthenticated ? (
         <a href="/login">
           <i class="fa fa-user-circle"></i>
           Sign In
         </a>
-      </div>
-    </nav>
-  )
-}
+      ) : (
+        <ul className="nav-list">
+          <li>
+            <a
+              className="nav-item"
+              href="/profile">
+              <i class="fa fa-user-circle"></i>
+              <p>{user.userName}</p>
+            </a>
+          </li>
+          <li>
+            <i class="fa fa-user-circle"></i>
+            <a href="/" onClick={handleLogout}>
+              <i class="fa fa-sign-out"></i>
+              Sign Out
+            </a>
+          </li>
+          </ul>
+          )}
+        </nav>
+      )
+      }
 
-export default Nav;
+      export default Nav;
